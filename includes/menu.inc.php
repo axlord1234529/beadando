a19b6757a8ffa51d9f282e6f432e09bc4982a4b0
Kalmár Sándor <sanyi1234529@gmail.com>
2022-11-06 22:59:28 +0100

    Created menu class

---
+++ includes/menu.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+Class Menu {
+    public static $menu = array(
+        'nyitolap'=>array('text'=>'Nyitólap','visible'=>array('when logged out'=>1,'when logged in'=>1)),
+        'belepes'=>array('text'=>'Belépés','visible'=>array('when logged out'=>1,'when logged in'=>0)),
+        'kilepes'=>array('text'=>'Kilépés','visible'=>array('when logged out'=>0,'when logged in'=>1)),
+
+    );
+
+    public static function getMenu($selectedPage)
+    {
+        $menu = "<ul>";
+
+        foreach (self::$menu as $pageUrl => $pageInfo)
+        {
+            if($_SESSION['userid'] == 0 && $pageInfo['visible']['when logged out'] )
+            {
+                $menu .= "<li><a href='".SITE_ROOT.$pageUrl."' ".($pageUrl==$selectedPage ? "class='selected'":"").">".$pageInfo['text']."</a></li>";
+            }elseif ($_SESSION['userid'] != 0 && $pageInfo['visible']['when logged in'] )
+            {
+                $menu .= "<li><a href='".SITE_ROOT.$pageUrl."' ".($pageUrl==$selectedPage ? "class='selected'":"").">".$pageInfo['text']."</a></li>";
+            }
+        }
+
+        $menu .="</ul>";
+
+        return $menu;
+    }
+}
