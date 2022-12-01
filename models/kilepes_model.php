f22d43438fcf2ba62bf92df9d3cd21ae6f3e382b
DANIÍ-LEGION\Dani <danzs717@gmail.com>
2022-12-01 22:55:22 +0100

    Created registration

---
+++ models/kilepes_model.php
@@ -0,0 +1,16 @@
+<?php
+
+class Kilepes_Model
+{
+	public function get_data()
+	{
+		$retData['eredmény'] = "OK";
+		$retData['uzenet'] = "Visszontlátásra kedves ".$_SESSION['userlastname']." ".$_SESSION['userfirstname']."!";
+		$_SESSION['userid'] =  0;
+		$_SESSION['userlastname'] =  "";
+		$_SESSION['userfirstname'] =  "";
+		return $retData;
+	}
+}
+
+?>
--- views/nyitolap_main.php
+++ views/nyitolap_main.php
@@ -1,1 +1,6 @@
-<strong>I'm the viw for nyitolap</strong>
+<div class="container">
+    <h1>Suti ktf</h1>
+        <p>
+            Hiszünk benne, hogy egy tökéletes nap minőségi, friss és ropogós péksüteményből készült reggelivel kezdődik. Szerintünk pékségben járni különleges élmény, ahol a pazar látvány mellett a pompás illatok csak fokozzák az étvágyat. Módszereinket 25 éve tökéletesítjük azért, hogy vásárlóink kifogástalan minőségű péksüteményeket fogyaszthassanak. A kenyérsütés örökzöld hagyományait ötvözzük az újhullámos pékségek innovatív ötleteivel. Minden pékterméket kézzel készítünk, kiemelt odafigyeléssel, a kifogástalan minőséget szem előtt tartva.
+        </p>
+</div>
